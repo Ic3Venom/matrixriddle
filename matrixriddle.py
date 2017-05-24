273136f8d6a33415f51e68b6b1a071b941203e41
Ic3Venom <Ic3Venom@gmail.com>
2017-05-23 23:48:56 -0700

    Update matrixriddle.py

--- matrixriddle.py
+++ matrixriddle.py
@@ -1,20 +1,67 @@
 import random
 
-def guess(numList, x, y):
-    for line in range(10):
-        pass
-
 def printMatrix(matrix):
     for line in matrix:
-        print line
+        print '\t', line
 
 def verify(numList):
-    for i in range(3):
-        sum = 0
-        for j in range(3):
-            sum += numList[j]
-        print 'sum', sum
+    firstTest = True
+    firstValue = 0
+    cases = []
     
+    #3 Cases: Horizontal, Vertical, Diagonal
+    #Case1
+    for i in range(3):
+        row = 0
+        
+        for j in range(3):
+            row += numList[i][j]
+
+        if firstTest == True:
+            firstValue = row
+            firstTest = False
+        else:
+            if row != firstValue:
+                firstValue = True
+                firstTest = 0
+                break
+                
+            elif i == 2:
+                cases.append(1)
+        
+    #Case2
+    for i in range(3):
+        column = 0
+        
+        for j in range(3):
+            column += numList[j][i]
+
+        if firstTest == True:
+            firstValue = column
+            firstTest = False
+        else:
+            if column != firstValue:
+                del firstTest
+                del firstValue
+                break
+                
+            elif i == 2:
+                cases.append(1)
+    #Case3
+    diagonal = [0,0]
+    
+    for i in range(3):
+        diagonal[0] += numList[i][i]
+    
+    for i in range(3):
+        diagonal[1] += numList[i][-i-1] 
+        
+    if diagonal[0] == diagonal[1]:
+        if len(cases) == 2:
+            return True
+        else:
+            return False
+
 def randomize():
     matrix  = [[0,0,0],[0,0,0],[0,0,0]]
     numList = [1,2,3,4,5,6,7,8,9]
@@ -23,46 +70,34 @@ def randomize():
         for j in range(3):
             randomIndex = random.randrange(len(numList))
             matrix[i][j] = numList[randomIndex]
-            print 'setup() for1;for1 (matrix[i][j])', matrix[i][j]
             
             del numList[randomIndex]
-            print 'setup() for1;for1 (len(numList))', len(numList)
-    print 'finished', matrix
     
     return matrix
-
-def swap(attempts):
-    #solves the riddle by replacing nearby values in the matrix
-
-    matrix = None
-    exit(1)
-    count = 0
-    x = 0
-    y = 0
-    
-    print matrix
-    printMatrix(matrix)
-
 
 def guess(attempts):
     #solves the riddle by guessing the correct matrix 'attempts' amount of times
     
-    matrix = setup()
-    for i in range(attemps):
-        matrix = setup()
-    count = 0
-    x = 0
-    y = 0
-    
-    print matrix
-    printMatrix(matrix)
+    for i in range(attempts):
+        print 'Attempt no.%d' % (i + 1)
+        matrix = randomize()
+        answer = verify(matrix)
+        
+        if answer == True:
+            return matrix
 
-
+    else:
+        print 'Unable to find solution in %d attempts. Terminating program...' % attempts
+        exit(1)
+        
 def main():
+    attempts = int( raw_input('How many attempts do you want to do? ') )
     
-    swap(10000)
-    guess(10000)
+    answer = guess(int(attempts))
 
+    print 'Answer:' 
+    printMatrix(answer)
+    
 if __name__ == '__main__':
     main()
     
